50aab8ce1215277e0642b71f34a87cdbc710d0cc
Bryce Sasser <sasserb@purdue.edu>
2021-11-11 19:35:39 -0500

    First draft of timer functions

--- src/timer.c
+++ src/timer.c
@@ -2,7 +2,12 @@
 #include "STM32f0xx.h"
 
 
-// TIM6 will be used as the refresh rate of the LEDs
+/*******************************************************************************
+Author: Bryce Sasser
+Date: 11/11/2021
+Description: initializes tim6 to trigger an interrupt at 30 Hz;
+            used as frame rate of the game
+*******************************************************************************/
 void init_TIM6(){
 
     //Turn on clock for TIM6
@@ -23,6 +28,43 @@ void init_TIM6(){
 
     return;
 }
+
+void TIM6_DAC_IRQHandler(void){
+    TIM6->SR &= ~TIM_SR_UIF;
+    return;
+}
+
+/*******************************************************************************
+Author: Bryce Sasser
+Date: 11/11/2021
+Description: initializes tim2 to trigger an interrupt at 2 Hz, which is
+                the speed of the game
+*******************************************************************************/
+void init_TIM2(){
+    //Turn on clock for TIM2
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+
+    //Turn on the interrupt trigger for timer
+    TIM2->DIER |= TIM_DIER_UIE;
+
+    //Set prescaler and auto-reload value to achieve 2 Hz
+    TIM2->PSC = 24000 - 1;
+    TIM2->ARR = 1000 - 1;
+
+    //Turn on the timer
+    TIM2->CR1 |= TIM_CR1_CEN;
+
+    //Unmask the interrupt
+    NVIC->ISER[0] |= 1 << TIM2_IRQn;
+
+    return;
+}
+
+void TIM2_IRQHandler(void){
+    TIM6->SR &= ~TIM_SR_UIF;
+    return;
+}
+
 /*******************************************************************************
 Author: Jonathon Snyder
 Date: 11/11/2021
